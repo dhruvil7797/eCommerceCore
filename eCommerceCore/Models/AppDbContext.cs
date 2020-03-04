e88d5ff30c8a7f05d9e2e3cfb808be353afcde6b
Ashish Panjwani <panjwani.ashish1993@gmail.com>
2020-03-04 14:05:26 -0500

    DB Connection added

---
+++ eCommerceCore/Models/AppDbContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace eCommerceCore.Models
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> opt) : base(opt) { }
+
+        protected override void OnModelCreating(ModelBuilder mb)
+        {
+            base.OnModelCreating(mb);
+        }
+
+        public DbSet<User> User { get; set; }
+    }
+}
