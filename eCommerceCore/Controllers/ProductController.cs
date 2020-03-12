dbac5614d18d81330609daa3b0fee727cb845788
dhruvil7797 <dhruvil7797@gmail.com>
2020-03-12 00:32:02 -0400

    Product Api Complete

--- eCommerceCore/Controllers/ProductController.cs
+++ eCommerceCore/Controllers/ProductController.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
-using eCommerceCore.Models
+using eCommerceCore.Models;
 
 namespace eCommerceCore.Controllers
 {
@@ -27,28 +27,48 @@ namespace eCommerceCore.Controllers
         public ProductsResponse Get()
         {
             var productsResponse = new ProductsResponse();
+            productsResponse.ListOfProducts = new List<Product>();
             productsResponse.Success = false;
-
-            var products = context.Product;
-            foreach(Product eachProduct in products)
+            foreach(Product eachProduct in context.Product.ToList())
             {
                 productsResponse.ListOfProducts.Add(eachProduct);
             }
-
+            productsResponse.Success = true;
             return productsResponse;
         }
 
         // GET: api/Product/5
-        [HttpGet("{id}", Name = "Get")]
-        public string Get(int id)
+        [HttpGet("{id}")]
+        public ProductsResponse Get(int id)
         {
-            return "value";
+            var productsResponse = new ProductsResponse();
+            productsResponse.ListOfProducts = new List<Product>();
+            productsResponse.Success = false;
+            var product = context.Product.FirstOrDefault(u => u.Id == id);
+            if (product != null)
+            {
+                productsResponse.ListOfProducts.Add(product);
+            }
+            productsResponse.Success = true;
+            return productsResponse;
         }
 
         // GET : api/Product/add
-        public void AddProduct()
+        [HttpGet("insert/All")]
+        public string AddProduct()
         {
-            
+            Product product1 = new Product("Product 1", "Description 1", 3.4, "Brand 1", "Url1", 1.2);
+            Product product2 = new Product("Product 2", "Description 2", 5.4, "Brand 1", "Url1", 0.7);
+            Product product3 = new Product("Product 3", "Description 3", 2.7, "Brand 1", "Url1", 2.2);
+            Product product4 = new Product("Product 4", "Description 4", 1.9, "Brand 1", "Url1", 1.2);
+            Product product5 = new Product("Product 5", "Description 5", 21.4, "Brand 1", "Url1", 4.2);
+            context.Add(product1);
+            context.Add(product2);
+            context.Add(product3);
+            context.Add(product4);
+            context.Add(product5);
+            context.SaveChanges();
+            return "Product Added Successfully";
         }
 
         
