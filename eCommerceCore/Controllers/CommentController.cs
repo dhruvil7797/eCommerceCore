1e67b662b5ed28c3917d0910f26f12abf49adcbb
dhruvil7797 <dhruvil7797@gmail.com>
2020-03-12 01:53:28 -0400

    Comment Controller Added

--- eCommerceCore/Controllers/CommentController.cs
+++ eCommerceCore/Controllers/CommentController.cs
@@ -22,27 +22,55 @@ namespace eCommerceCore.Controllers
 
         // GET: api/Comment/5
         [HttpGet("{id}", Name = "Get")]
-        public string Get(int id)
+        public FetchCommentResponse Get(int id)
         {
-            return "value";
+            var response = new FetchCommentResponse();
+            response.Success = false;
+            response.ListOfComments = new List<Comment>();
+            response.AvgRating = 0;
+            foreach(var comments in context.Comment.Where(u => u.ProductId == id).ToList())
+            {
+                response.ListOfComments.Add(comments);
+                response.AvgRating = comments.Ratings;
+            }
+            response.AvgRating = (float)response.AvgRating/response.ListOfComments.Count;
+            response.Success = true;
+            return response;
         }
 
         // POST: api/Comment
         [HttpPost]
-        public void Post([FromBody] string value)
+        public AddCommentResponse Post([FromBody] Comment commentArg)
         {
+            var response = new AddCommentResponse();
+            response.Success = false;
+            if(context.OrderRecord.Where(o => o.ProductVariationID == commentArg.ProductId && o.UserId == commentArg.UserId).Count() != 0)
+            {
+                context.Comment.Add(commentArg);
+                context.SaveChanges();
+                response.Success = true;
+                response.Message = "Comment Added Successfully";
+            }
+            else
+            {
+                response.Message = "User has not purchase this product. Cannot comment";
+            }
+            return response;
         }
 
         public class AddCommentResponse
         {
             public bool Success { get; set; }
-            public int commentId { get; set; }
+            public string Message { get; set; }
         }
 
         public class FetchCommentResponse
         {
             public bool Success { get; set; }
-            public List<Comment> ListOfProducts { get; set; }
+            public List<Comment> ListOfComments { get; set; }
+            public float AvgRating { get; set; }
+
         }
+
     }
 }
